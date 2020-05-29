7cca2e239627c3c2287f91a36062ed890eed7484
XiangyiZhao <50124074+XiangyiZhao@users.noreply.github.com>
2020-05-29 18:41:01 +0800

    Update Sobel_PSNR.py

--- Sobel_PSNR.py
+++ Sobel_PSNR.py
@@ -3,19 +3,20 @@ import numpy as np
 import math
 import imageio
 
-#================================================================================================================================================
-#Read the original and the processed image
-#================================================================================================================================================
 path_org = "home.jpg"                                               # Your image path 
 path_edge = "home_float.jpg"
 img_org = Image.open(path_org)
-img_edge = Image.open(path_edge)
 width, height = img_org.size
-int MSE = 0;
+print(width, height)
+img_edge = Image.open(path_edge)
+np_img_org = np.asarray(img_org)
+np_img_edge = np.asarray(img_edge)
+MSE = 0
 
 for x in range(0, height):
   for y in range(0, width):
-    MSE += (img_org[x][y][0]+img_org[x][y][1]+img_org[x][y][2]-img_edge[x][y])^2
-    
+    org_intensity = np_img_org[x][y][0]+np_img_org[x][y][1]+np_img_org[x][y][2]
+    MSE += (org_intensity-np_img_edge[x][y])**2
+
 MSE_final = MSE/(height*width)
 print(MSE_final)
